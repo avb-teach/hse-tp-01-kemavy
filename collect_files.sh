d83b34ce4b22d18da0679e659729505091334c28
kemavy <kretovame-520@yandex.ru>
2025-04-28 21:01:43 +0300

    hw

---
+++ collect_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+input_dir = "$1"
+output_dir = "$2"
+max_depth = 0
+if ["$3" == "--max_depth"];
+	then max_depth = "$4"
+fi
+func() {
+	cur = "$1"
+	cur_depth = "$2"
+
+	if ["$max_depth" -ge "$cur_depth" ] && ["$max_depth" -ne 0];
+		then return
+	fi
+	for file in "$cur"/*;
+		do if [-f  "$item"];
+			then  name = $(basename "$file")
+			count = 1
+			while  [-e "$output_dir/$name"];
+				do name = "${name%.*}_$count.${name##*.}"
+				count = $(count + 1)
+			done
+			cp "$file" "$output_dir/$name"
+		else;
+			then func "$file" $(cur_depth + 1) 
+`		fi
+	done
+}
+
+func "$input_dir" 0
